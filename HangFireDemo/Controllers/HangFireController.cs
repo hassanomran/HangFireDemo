ebbe896f1b94c9edbd9caffd829a2b982d5d9545
User NA <User NA@NAQEEB-AZIZI>
2025-01-30 17:02:01 +0200

    AddHangFire job :fire and Forhet

---
+++ HangFireDemo/Controllers/HangFireController.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangFireController : ControllerBase
+    {
+        [HttpGet("welcome")] // Make sure this is specified to handle the "welcome" endpoint
+        public IActionResult Get()
+        {
+            var JobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome To Our App"));
+            return Ok($"JobId {JobId}: Welcome Email Sent to the user");
+        }
+
+        public void SendWelcomeEmail(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+    }
+}
