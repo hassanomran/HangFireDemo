f914e65f2973cb6c26c901df82528c1b7a4503b0
User NA <User NA@NAQEEB-AZIZI>
2025-02-02 17:19:22 +0200

    Adding many type of jobs

--- HangFireDemo/Controllers/HangFireController.cs
+++ HangFireDemo/Controllers/HangFireController.cs
@@ -4,19 +4,41 @@ using Microsoft.AspNetCore.Mvc;
 namespace HangFireDemo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/hangfire")]
+    [Route("hangfire")]  // Defines base route as /hangfire
     public class HangFireController : ControllerBase
     {
-        [HttpGet("welcome")] // Make sure this is specified to handle the "welcome" endpoint
-        public IActionResult Get()
+        [HttpGet("welcome")]  
+        public IActionResult welcome()
         {
-            var JobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome To Our App"));
-            return Ok($"JobId {JobId}: Welcome Email Sent to the user");
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome To Our App"));
+            return Ok($"JobId {jobId}: Welcome Email Sent to the user");
+        }
+        [HttpGet("Discount")]
+        public IActionResult Discount()
+        {
+            int timeInSeconds = 30;
+            var jobId = BackgroundJob.Schedule(() => SendWelcomeEmail("Welcome to our app"), TimeSpan.FromSeconds(timeInSeconds));
+            return Ok($"Job ID: {jobId}. Discount email will be sent in 30 seconds!");
+        }
+        [HttpGet("DatabaseUpdate")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database updated"), Cron.Minutely);
+            return Ok("Database check job initiated!");
+        }
+        [HttpGet("Confirm")]
+        public IActionResult Confirm()
+        {
+            int timeInSeconds = 5;
+            var ParentjobId = BackgroundJob.Schedule(() => SendWelcomeEmail("You Asked to Unsusbribe"), TimeSpan.FromSeconds(timeInSeconds));
+            BackgroundJob.ContinueJobWith(ParentjobId, () => SendWelcomeEmail("You are unsusbribed now !"));
+            return Ok("Database check job initiated!");
         }
 
-        public void SendWelcomeEmail(string Text)
+        public void SendWelcomeEmail(string text)
         {
-            Console.WriteLine(Text);
+            Console.WriteLine(text);
         }
     }
 }
